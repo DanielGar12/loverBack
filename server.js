411aa300fd2eefe132497614f16b638566198213
DanielGar12 <garcia.daniel23256@gmail.com>
2024-10-15 12:25:08 -0500

    made server runnable

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-console.log('Hello world')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('Mongo successfully connected'))
+.catch(err => console.error('error starting the server', err))
+
+app.use(express.json())
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/users', async (req, res) => {
+    const {firstName, lastName, username, password} = req.body;
+
+    try{
+
+    }
+    catch(error){
+        
+    }
+})
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
